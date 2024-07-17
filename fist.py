ba7b5ad8ae9e20297dc08164a9f778681cf4fcac
Ko Ikudome <koi@auriq.com>
2024-07-17 16:00:56 -0700

    n1

--- fist.py
+++ fist.py
@@ -3,21 +3,13 @@ import os
 new one two
 
 master 1
+new line 1
 
 #2nd comment
 msg = 'hello world c1 c3'
 print(msg)
 
 master 1 2
-
-change local 1 2 33
-change_at_github 2 3 4 5 6
-
-Branch out 1
-
-branch b2 1 2 3 4 5 6 7
-
-branch b1 1 2 3
 
 branch b3 1
 
